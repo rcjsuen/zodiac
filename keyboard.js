e5d46cf5d50058bd795db1e2c49e1a0ce3595b2e
Remy Suen <remy.suen@gmail.com>
2015-12-21 06:26:47 -0500

    Add a 'shift' key to the keyboard
    When the shift key is pressed, all the characters will be rendered in
    uppercase instead. Unlike the other keys, pressing the shift key will
    not cause the text of that key to be enlarged. It will however change
    colours to indicate that it is being pressed.

--- keyboard.js
+++ keyboard.js
@@ -2,16 +2,35 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-function drawKey(x, y, letter, keydown) {
+var shiftDown = false;
+var keydown = [
+	[ false, false, false, false, false, false, false, false, false, false ],
+	[ false, false, false, false, false, false, false, false, false ],
+	[ false, false, false, false, false, false, false, false, false ]
+];
+var keyboard = [
+	[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ],
+	[ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ],
+	[ 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.' ]
+];
+
+function drawKeyOutline(x, y) {
 	ctx.beginPath();
 	ctx.strokeStyle = "black";
 	ctx.strokeRect(x, y, 60, 60);
 	ctx.closePath();
+}
 
+function drawKey(x, y, letter, keydown) {
+	drawKeyOutline(x, y);
 	drawCharacter(x, y, 60, 60, letter, keydown);
 }
 
 function drawCharacter(x, y, height, width, letter, keydown) {
+	if (shiftDown) {
+		letter = letter.toUpperCase();	
+	}
+	
 	ctx.beginPath();
 	ctx.font = keydown ? "40pt Verdana" : "20pt Verdana";
 	ctx.textAlign = "center";
@@ -21,17 +40,17 @@ function drawCharacter(x, y, height, width, letter, keydown) {
 	ctx.closePath();
 }
 
-var keydown = [
-	[ false, false, false, false, false, false, false, false, false, false ],
-	[ false, false, false, false, false, false, false, false, false ],
-	[ false, false, false, false, false, false, false, false, false ]
-];
+function drawShiftKey(x, y) {
+	drawKeyOutline(x, y);
 
-var keyboard = [
-	[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ],
-	[ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ],
-	[ 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.' ]
-];
+	ctx.beginPath();
+	ctx.font = "15pt Verdana";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = shiftDown ? "red" : "blue";
+	ctx.fillText("shift", (x + x + 60) / 2, (y + y + 60) / 2);
+	ctx.closePath();
+}
 
 function drawKeyboard() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -43,6 +62,8 @@ function drawKeyboard() {
 	for (var i = 0; i < keyboard[1].length; i++) {
 		drawKey(35 + 70 * i + (10), 80, keyboard[1][i], keydown[1][i]);
 	}
+	
+	drawShiftKey(10, 150);
 
 	for (var i = 0; i < keyboard[2].length; i++) {
 		drawKey(70 + 70 * i + (10), 150, keyboard[2][i], keydown[2][i]);
@@ -52,22 +73,32 @@ function drawKeyboard() {
 }
 
 function keyUpHandler(e) {
-	for (var i = 0; i < keyboard.length; i++) {
-		for (var j = 0; j < keyboard[i].length; j++) {
-			if (keyboard[i][j] === e.key) {
-				keydown[i][j] = false;
-				return;
+	if (e.key === "Shift") {
+		shiftDown = false;
+	} else {
+		var key = e.key.toLowerCase();
+		for (var i = 0; i < keyboard.length; i++) {
+			for (var j = 0; j < keyboard[i].length; j++) {
+				if (keyboard[i][j] === key) {
+					keydown[i][j] = false;
+					return;
+				}
 			}
 		}
 	}
 }
 
 function keyDownHandler(e) {
-	for (var i = 0; i < keyboard.length; i++) {
-		for (var j = 0; j < keyboard[i].length; j++) {
-			if (keyboard[i][j] === e.key) {
-				keydown[i][j] = true;
-				return;
+	if (e.key === "Shift") {
+		shiftDown = true;
+	} else {
+		var key = e.key.toLowerCase();
+		for (var i = 0; i < keyboard.length; i++) {
+			for (var j = 0; j < keyboard[i].length; j++) {
+				if (keyboard[i][j] === key) {
+					keydown[i][j] = true;
+					return;
+				}
 			}
 		}
 	}
