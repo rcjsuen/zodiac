6dbc6c65bd5bca09356e8111429c2eb6e6a4dbd8
Remy Suen <remy.suen@gmail.com>
2015-12-21 03:44:35 -0500

    Initial commit of the flashcard application
    This is the first pass of the flashcard application. Given a file that
    alternates between a line with an English word and a line with its
    Japanese meaning, the program will prompt the user to enter in the
    corresponding English word.
    
    zodiac.js contains the core code for parsing an input file and
    manipulating the HTML file to be like a flashcard.
    
    keyboard.js is visual representation of a QWERTY keyboard backed by the
    HTML5 Canvas.

---
+++ keyboard.js
@@ -0,0 +1,78 @@
+/*eslint-env browser */
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function drawKey(x, y, letter, keydown) {
+	ctx.beginPath();
+	ctx.strokeStyle = "black";
+	ctx.strokeRect(x, y, 60, 60);
+	ctx.closePath();
+
+	drawCharacter(x, y, 60, 60, letter, keydown);
+}
+
+function drawCharacter(x, y, height, width, letter, keydown) {
+	ctx.beginPath();
+	ctx.font = keydown ? "40pt Verdana" : "20pt Verdana";
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = keydown ? "red" : "blue";
+	ctx.fillText(letter, (x + x + width) / 2, (y + y + height) / 2);
+	ctx.closePath();
+}
+
+var keydown = [
+	[ false, false, false, false, false, false, false, false, false, false ],
+	[ false, false, false, false, false, false, false, false, false ],
+	[ false, false, false, false, false, false, false ]
+];
+
+var keyboard = [
+	[ 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p' ],
+	[ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l' ],
+	[ 'z', 'x', 'c', 'v', 'b', 'n', 'm' ]
+];
+
+function drawKeyboard() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	for (var i = 0; i < keyboard[0].length; i++) {
+		drawKey(70 * i + (10), 10, keyboard[0][i], keydown[0][i]);
+	}
+
+	for (var i = 0; i < keyboard[1].length; i++) {
+		drawKey(35 + 70 * i + (10), 80, keyboard[1][i], keydown[1][i]);
+	}
+
+	for (var i = 0; i < keyboard[2].length; i++) {
+		drawKey(70 + 70 * i + (10), 150, keyboard[2][i], keydown[2][i]);
+	}
+
+	requestAnimationFrame(drawKeyboard);
+}
+
+function keyUpHandler(e) {
+	for (var i = 0; i < keyboard.length; i++) {
+		for (var j = 0; j < keyboard[i].length; j++) {
+			if (keyboard[i][j] === e.key) {
+				keydown[i][j] = false;
+				return;
+			}
+		}
+	}
+}
+
+function keyDownHandler(e) {
+	for (var i = 0; i < keyboard.length; i++) {
+		for (var j = 0; j < keyboard[i].length; j++) {
+			if (keyboard[i][j] === e.key) {
+				keydown[i][j] = true;
+				return;
+			}
+		}
+	}
+}
+
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);
+drawKeyboard();
---
+++ zodiac.js
@@ -0,0 +1,81 @@
+/*eslint-env browser */
+var english = null;
+var japanese = null;
+
+var count = -1;
+var remaining = -1;
+var idx = -1;
+
+var files = null;
+var fileCounter = 0;
+var wordCounter = 0;
+
+function handleFileSelect(e) {
+	wordCounter = 0;
+	fileCounter = 0;
+	english = [];
+	japanese = [];
+	files = e.target.files;
+	var reader = new FileReader();
+	
+	reader.onload = function(e) {
+		var text = reader.result;
+		var strings = text.split("\r");
+		for (var i = 0; i < strings.length / 2; i++) {
+			english[wordCounter] = strings[i * 2].trim();
+			japanese[wordCounter] = strings[(i * 2) + 1].trim();
+			wordCounter++;
+		}
+		count = english.length;
+		remaining = count;
+
+		fileCounter++;
+
+		if (fileCounter !== files.length) {
+			reader.readAsText(files[fileCounter], "UTF-8");
+		} else {
+			show();
+		}
+	};
+
+	reader.readAsText(files[fileCounter], "UTF-8");
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function show() {
+	idx = Math.floor(Math.random() * remaining);
+	document.getElementById("japanese").innerHTML = japanese[idx];
+	document.getElementById("remaining").innerHTML = "残り： " + remaining;
+}
+
+function onClick() {
+	var input = document.getElementById("input");
+	if (input.value === english[idx]) {
+		remaining--;
+		english[idx] = english[remaining];
+		japanese[idx] = japanese[remaining];
+
+		document.getElementById("mistake").innerHTML = "";
+		input.value = "";
+
+		if (remaining === 0) {
+			alert("終わりました！");
+			document.getElementById("japanese").innerHTML = "";
+			document.getElementById("remaining").innerHTML = "残り： 0";
+			document.getElementById("nextBtn").disabled = true;
+		} else {
+			show();
+		}
+	} else {
+		document.getElementById("mistake").innerHTML = "<font color=\"red\">間違った！</font>";
+	}
+}
+
+function keyDownHandler(e) {
+	if (e.key === "Enter" && remaining !== 0) {
+		onClick();
+	}
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
